a9fee91633772543b11b895e9b047317e5e89eb4
Mario Hernandez <mhmario963@gmail.com>
2024-09-05 22:16:28 -0400

    implement exercise one.

--- ch4/ch4.go
+++ ch4/ch4.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
-	fmt.Println("hi")
+	exerciseOne()
+}
+
+func exerciseOne() {
+	nums := make([]int, 0, 100)
+	for i := 0; i < 100; i++ {
+		nums = append(nums, rand.Intn(101))
+	}
+	fmt.Println(nums)
 }
