08c0e2d21919e23d127305a11bf1a23883f329eb
Mario Hernandez <mhmario963@gmail.com>
2024-09-05 23:04:07 -0400

    implement exercise 3.

--- ch4/ch4.go
+++ ch4/ch4.go
@@ -8,6 +8,19 @@ import (
 func main() {
 	exerciseOne()
 	exerciseTwo()
+
+	// Exercise 3:
+	// total := 0
+	// for i := 0; i < 10; i++ {
+	// 	total := total + i
+	// 	fmt.Println(total)
+	// }
+	// fmt.Println(total)
+	// Explanation: the variable named total that is declared inside the for loop has a scope that only exists
+	// within the body of the for loop. The variable named total that is used on the right hand side of the
+	// declaration+assignment operation, `:=`, is from the main body scope. As you can see we never update it.
+	// Lastly when we print total after the for loop it is the same variable from the beggining of the main
+	// function and thus we never update it which is why it will print out zero.
 }
 
 func exerciseOne() {
