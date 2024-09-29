e22dfa7df0a1f79d5d55dedd6705c0fe77c87720
Mario Hernandez <mhmario963@gmail.com>
2024-09-29 19:28:23 -0400

    signal a cancel via shared done variable.

--- concurrency/ch1/ch1.go
+++ concurrency/ch1/ch1.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+var done bool
+var mu sync.Mutex
+
 func main() {
+	time.Sleep(time.Second)
+	println("started")
 	go periodic()
 	time.Sleep(time.Second * 10)
+	mu.Lock()
+	done = true
+	mu.Unlock()
+	println("cancelled")
+	time.Sleep(3 * time.Second)
 }
 
 func periodic() {
@@ -16,5 +27,11 @@ func periodic() {
 		fmt.Printf("tick %d\n", i)
 		i++
 		time.Sleep(time.Second)
+		mu.Lock()
+		if done {
+			return
+		}
+		mu.Unlock()
+
 	}
 }
