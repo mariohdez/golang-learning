6c3ef89b3ee1f27952b04905b8b79d00f8640e80
Mario Hernandez <mhmario963@gmail.com>
2024-09-29 21:02:02 -0400

    dont corrupt counter. gotta sync writes w/ locks.

--- concurrency/ch1/ch1.go
+++ concurrency/ch1/ch1.go
@@ -1,37 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"sync"
 	"time"
 )
 
-var done bool
-var mu sync.Mutex
-
 func main() {
-	time.Sleep(time.Second)
-	println("started")
-	go periodic()
-	time.Sleep(time.Second * 10)
-	mu.Lock()
-	done = true
-	mu.Unlock()
-	println("cancelled")
-	time.Sleep(3 * time.Second)
-}
+	counter := 0
+	mu := sync.Mutex{}
 
-func periodic() {
-	i := 0
-	for {
-		fmt.Printf("tick %d\n", i)
-		i++
-		time.Sleep(time.Second)
-		mu.Lock()
-		if done {
-			return
-		}
-		mu.Unlock()
-
+	for i := 0; i < 1000; i++ {
+		go func() {
+			mu.Lock()
+			defer mu.Unlock()
+			counter = counter + 1
+		}()
 	}
+	time.Sleep(5 * time.Second)
+	mu.Lock()
+	println(counter)
+	mu.Unlock()
 }
